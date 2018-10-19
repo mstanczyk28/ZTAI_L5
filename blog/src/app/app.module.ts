1910089968bedc130ee13cdbb8f6ddc993c81901
Monika Stanczyk <monika.maria.stanczyk@gmail.com>
2018-10-19 23:20:29 +0200

    blog - kopiowanie plikow

---
+++ blog/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    QuizComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
