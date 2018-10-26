825901ae03774287be3bb24ea7695df5edfe6f02
Monika Stanczyk <monika.maria.stanczyk@gmail.com>
2018-10-26 12:14:32 +0200

    tworzenie bloga

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,22 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { QuizComponent } from './components/quiz/quiz.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {QuizComponent} from './components/quiz/quiz.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponentComponent} from './home-component/home-component.component';
+import {BlogComponent} from "./components/blog/blog.component";
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {BlogItemComponent} from './components/blog-item/blog-item.component';
+import {BlogItemTextComponent} from './components/blog-item-text/blog-item-text.component';
+import {BlogItemImageComponent} from './components/blog-item-image/blog-item-image.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponentComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  }];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    QuizComponent
+    QuizComponent,
+    HomeComponentComponent,
+    BlogComponent,
+    NavbarComponent,
+    BlogItemComponent,
+    BlogItemTextComponent,
+    BlogItemImageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
