bdd681647471a5d1306dd9b1dfd661c4392df11f
Monika Stanczyk <monika.maria.stanczyk@gmail.com>
2018-11-07 19:13:04 +0100

    lab 5 cd

--- blog/src/app/components/blog-item-text/blog-item-text.component.ts
+++ blog/src/app/components/blog-item-text/blog-item-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-blog-item-text',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogItemTextComponent implements OnInit {
 
+  @Input('text') text : string;
+  @Output() titleChanged = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
