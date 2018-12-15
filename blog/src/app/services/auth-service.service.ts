69ad9fd018094074455fdfb711b4f5d35379128f
Monika Stanczyk <monika.maria.stanczyk@gmail.com>
2018-12-15 10:23:22 +0100

    koniec tai

--- blog/src/app/services/auth-service.service.ts
+++ blog/src/app/services/auth-service.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Token} from "@angular/compiler";
+// import {Token} from "@angular/compiler";
+import {JwtHelper} from "angular2-jwt";
+import {Token} from '../models/token';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,31 +12,35 @@ export class AuthServiceService {
 
   private url = 'http://localhost:3000/api';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   authenticate(credentials) {
     return this.http.post(this.url + '/user/auth', {
       login: credentials.login,
       password: credentials.password
-    }).map((result: Token) => {
-      if (result && result.token) {
-        localStorage.setItem('token', result.token);
-        return true;
-      }
-      return false;
-    }).catch(error.handle);
+    }).pipe(
+      map((result: Token) => {
+        if (result && result.token) {
+          localStorage.setItem('token', result.token);
+          return true;
+        }
+        return false;
+      })
+    );
   }
 
   createOrUpdate(credentials) {
-    return this.http.post(this.url + '/user/create', credentials)
-      .catch(error.handle);
+    return this.http.post(this.url + '/user/create', credentials);
   }
 
   logout() {
     return this.http.delete(this.url + '/user/logout/' + this.currentUser.userId)
-      .map(() => {
-        localStorage.removeItem('token');
-      });
+      .pipe(
+        map(() => {
+          localStorage.removeItem('token');
+        })
+      );
   }
 
   isLoggedIn() {
@@ -41,23 +48,21 @@ export class AuthServiceService {
     const token = localStorage.getItem('token');
     if (!token) {
       return false;
-
-    }}
-  return !(jwtHelper.isTokenExpired(token));
-
-
-get currentUser() {
-  const token = this.getToken();
-  if (!token) {
-    return null;
+    }
+    return !(jwtHelper.isTokenExpired(token));
   }
 
-  return new JwtHelper().decodeToken(token);
-}
 
-getToken() {
-  return localStorage.getItem('token');
-}
+  get currentUser() {
+    const token = this.getToken();
+    if (!token) {
+      return null;
+    }
 
-}
+    return new JwtHelper().decodeToken(token);
+  }
 
+  getToken() {
+    return localStorage.getItem('token');
+  }
+}
